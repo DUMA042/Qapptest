e7bc8d66b8425f13b2aad2f2687a5a698dada3ce
BurakEken <burakeken1907@hotmail.com>
2022-03-27 17:58:27 +0300

    This branch is focus on the implementation of the User Interface using the Jetpack compose design library--> This branch has added new folders and some new classes

--- app/src/main/java/com/example/qapptest/MainActivity.kt
+++ app/src/main/java/com/example/qapptest/MainActivity.kt
@@ -4,25 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.qapptest.UIelement.HouseScreens
 import com.example.qapptest.ui.theme.QapptestTheme
 
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
+
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+       setContent {
             QapptestTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    HouseScreens()
                 }
             }
         }
---
+++ app/src/main/java/com/example/qapptest/UIelement/MainControlCenter.kt
@@ -0,0 +1,8 @@
+package com.example.qapptest.UIelement
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainControlCenter() {
+
+}
