73db1272a8add96cc3146e32d4958545fcf4d6ce
BurakEken <burakeken1907@hotmail.com>
2022-03-29 18:28:30 +0300

    This branch is focus on the implementation of the User Interface using the Jetpack compose design library for the signup and homepage Navigation is been worked on

--- app/src/main/java/com/example/qapptest/UIelement/MainControlCenter.kt
+++ app/src/main/java/com/example/qapptest/UIelement/MainControlCenter.kt
@@ -1,6 +1,10 @@
 package com.example.qapptest.UIelement
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainControlCenter() {
